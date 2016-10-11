4556c01f5b43bcccf9d6344b4ca3a0ba9ad2ca9b
Bernardo Belchior <bernardo@wifi-interno.fe.up.pt>
2016-10-11 12:35:25 +0100

    Trying to display board using axial coordinates.

--- tp1/display_board.pl
+++ tp1/display_board.pl
@@ -1,41 +1,70 @@
 start:-
-  board(Board, Width),
-  display_board(Board, Width).
+  board(Board),
+  display_board(Board, 0).
 
-board(Board, Width):-
-  Board = [['A', 'A', 'A'],
-          ['A', 'A', 'A', 'A'],
-          ['A', 'A', 'A', 'A', 'A'],
-          ['A', 'A', 'A', 'A'],
-          ['A', 'A', 'A']],
-  Width is 5.
 
-display_board([], Width).
-display_board([Even | Rest], Width):-
-  indent_line(Even, Width),
+board(Board):-
+  Board = [
+  ['', '', '', '', 'O', 'O', 'O', 'O', '', '', '', ''],
+  ['', '', '', '', 'O', 'O', 'X', 'O', 'O', 'O', 'O', ''],
+  ['', '', '', 'O', 'O', 'O', 'O', 'O', 'O', 'W', 'O', ''],
+  ['', '', '', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ''],
+  ['', '', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ''],
+  ['', '', '', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', ''],
+  ['', '', 'O', 'O', 'O', 'X', 'O', 'O', 'O', 'O', ' ', ''],
+  ['', '', 'O', 'O', 'O', 'O', 'O', 'O', 'W', 'O', 'O', ''],
+  ['', '', '', '', '', 'O', 'O', 'O', 'O', '', '', '']
+  ].
+
+/*board(Board):-
+  Board = [
+          ['O', 'O', 'O', 'O', ' ', ' ', ' ', ' '],
+          ['O', 'O', 'O', 'X', 'O', 'O', 'O', ''],
+          ['O', 'O', 'O', 'O', 'O', 'W', 'O', ' '],
+          ['O', 'W', 'O', 'O', 'O', 'O', 'O', ' '],
+          ['O', 'O', 'O', 'O', 'O', 'O', 'O', ' '],
+          [' ', 'O', 'O', 'O', 'O', 'O', 'X', 'O'],
+          ['O', 'X', 'O', 'O', 'O', 'O', 'O', ' '],
+          [' ', 'O', 'O', 'O', 'W', 'O', 'O', 'O'],
+          [' ', ' ', ' ', 'O', 'O', 'O', 'O', ' ']].*/
+
+display_board([], Offset).
+display_board([Even | Rest], Offset):-
+  /*indent_line(Even, Width),*/
+  space(Offset),
   display_even_line(Even), nl,
-  display_rest(Rest, Width).
+  display_rest(Rest, Offset).
 
-display_rest([], Width).
-display_rest([Odd | Rest], Width):-
-  indent_line(Odd, Width),
+display_rest([], Offset).
+display_rest([Odd | Rest], Offset):-
+  /*indent_line(Odd, Width),*/
+  space(Offset),
   display_odd_line(Odd), nl,
-  display_board(Rest, Width).
+  Offset1 is Offset+1,
+  display_board(Rest, Offset1).
 
 display_odd_line([]).
 display_odd_line([Current | Rest]):-
-  write(' '), write(Current),
+  write(Current), write(' '),
   display_odd_line(Rest).
 
 display_even_line([]).
 display_even_line([Current | Rest]):-
-  write(Current), write(' '),
+  write(' '), write(Current),
   display_even_line(Rest).
 
-indent_line([Line | Rest], Width):-
+  space(0).
+  space(Count):-
+    write('  '),
+    C1 is Count - 1,
+  space(C1).
+
+/*indent_line([Line | Rest], Width):-
   get_line_width([Line | Rest], LineWidth),
   Spaces is round(Width - LineWidth/2),
   space(Spaces).
+
+
 
 space(0).
 space(Count):-
@@ -46,4 +75,4 @@ space(Count):-
 get_line_width([], 0).
 get_line_width([Element | Rest], LineWidth):-
   get_line_width(Rest, LineWidth1),
-  LineWidth is LineWidth1 + 1.
+  LineWidth is LineWidth1 + 1.*/
