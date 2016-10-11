f1ab05234b14c69ae7bad6dc1d350ea523e45d3b
epassos26 <epassos26@gmail.com>
2016-10-11 17:00:47 +0100

    Added list_utils.pl

---
+++ tp1/list_utils.pl
@@ -0,0 +1,22 @@
+list_nth_element([X|Xs],0,X).
+list_nth_element([Y|Ys],N,X) :- N1 is N-1, list_nth_element(Ys,N1,X).
+
+list_delete_all([X|Xs],X,Ys) :- list_delete_all(Xs,X,Ys).
+list_delete_all([X|Xs],Z,[X|Ys]) :- X \= Z,list_delete_all(Xs,Z,Ys).
+list_delete_all([],X,[]).
+
+list_delete_nth([X|Xs],0,Xs).
+list_delete_nth([X|Xs],N,[X|Ys]) :- N1 is N-1, list_delete_nth(Xs,N1,Ys).
+
+list_append([],Ys,Ys).
+list_append([X|Xs],Ys,[X|Zs]) :- append(Xs,Ys,Zs).
+
+list_slice([X|Xs],0,Xs).
+list_slice([X|Xs],N,Zs) :- N1 is N-1, list_slice(Xs,N1,Zs).
+
+list_replace_all([X|Xs],X,Z,[Z|Zs]) :- list_replace_all(Xs,X,Z,Zs).
+list_replace_all([X|Xs],Y,Z,[X|Zs]) :- list_replace_all(Xs,Y,Z,Zs).
+list_replace_all([],Y,Z,[]).
+
+list_replace_nth([X|Xs],0,Z,[Z|Xs]).
+list_replace_nth([X|Xs],N,Z,[X|Zs]) :- N1 is N-1, list_replace_nth(Xs,N1,Z,Zs).
