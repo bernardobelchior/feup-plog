8557025a8ed916aa2dca1d36d2e719e232f30586
epassos26 <epassos26@gmail.com>
2016-11-13 16:19:53 +0000

    AI now moves ships and places buildings

--- tp1/logic/easy_cpu.pl
+++ tp1/logic/easy_cpu.pl
@@ -1,14 +1,16 @@
+%to be able to use the random function
+:- use_module(library(random)).
+
+%basic movement
 easy_cpu_select_ship_movement(Board, Ships, TradeStations, Colonies, Wormholes, NumPlayers, NumShipsPerPlayer, CurrentPlayer, NewShips, ShipNo):-
   easy_cpu_select_ship(NumShipsPerPlayer, ShipNo),
-  select_ship_direction(Direction),
+  easy_cpu_select_ship_direction(ReadDirection),
+  number_to_direction(ReadDirection,Direction),
   get_piece_position(Ships, CurrentPlayer, ShipNo, ShipPosition),
   easy_cpu_do_appropriate_move(Board, Ships, TradeStations, Colonies, Wormholes, NumPlayers, NumShipsPerPlayer, CurrentPlayer, ShipNo, ShipPosition, Direction, NewShips).
 
  easy_cpu_select_ship_movement(Board, Ships, TradeStations, Colonies, Wormholes, NumPlayers, NumShipsPerPlayer, CurrentPlayer, NewShips, _ShipNo) :-
   easy_cpu_select_ship_movement(Board, Ships, TradeStations, Colonies, Wormholes, NumPlayers, NumShipsPerPlayer, CurrentPlayer, NewShips, _SelectedShipNo).
-
-easy_cpu_select_ship(NumShipsPerPlayer, ShipNo) :-
-    random(0,NumShipsPerPlayer,ShipNo).
 
 easy_cpu_do_appropriate_move(Board, Ships, TradeStations, Colonies, Wormholes, NumPlayers, NumShipsPerPlayer, CurrentPlayer, ShipNo, ShipPosition, Direction, NewShips):-
     is_move_to_wormhole(ShipPosition, Direction, Wormholes, InWormhole),
@@ -18,22 +20,50 @@ easy_cpu_do_appropriate_move(Board, Ships, TradeStations, Colonies, Wormholes, _
   easy_cpu_select_ship_num_tiles(NumTiles),
   move_ship_if_valid(Board, Ships, TradeStations, Colonies, Wormholes, CurrentPlayer, ShipNo, ShipPosition, Direction, NumTiles, NewShips).
 
+%Wormhole movement
 easy_cpu_move_through_wormhole(Board, Ships, TradeStations, Colonies, Wormholes, _NumPlayers, _NumShipsPerPlayer, CurrentPlayer, ShipNo, _ShipPosition, _Direction, NewShips, InWormhole):-
-    easy_cpu_select_wormhole_exit(NumWormholes, InWormhole, SelectedOutWormhole),
-    number_to_wormhole(Wormholes,SelectedOutWormhole, OutWormhole),
+    easy_cpu_select_wormhole_exit(NumWormholes, InWormhole, OutWormhole),
     move_ship(Ships, OutWormhole, CurrentPlayer, ShipNo, west, 0, TmpShips),
-    display_ship_direction_info(ShipNo),
     easy_cpu_select_ship_direction(TmpDirection),
     move_ship_if_valid(Board, TmpShips, TradeStations, Colonies, Wormholes, CurrentPlayer, ShipNo, OutWormhole, TmpDirection, 1, NewShips).
 
 move_through_wormhole(Board, Ships, TradeStations, Colonies, Wormholes, NumPlayers, NumShipsPerPlayer, CurrentPlayer, ShipNo, ShipPosition, Direction, NewShips, InWormhole):-
     move_through_wormhole(Board, Ships, TradeStations, Colonies, Wormholes, NumPlayers, NumShipsPerPlayer, CurrentPlayer, ShipNo, ShipPosition, Direction, NewShips, InWormhole).
 
-select_ship_action(Ships, PlayerNo, ShipNo,TradeStations, Colonies,  NewTradeStations, NewColonies) :-
-  display_action_info,
-  select_action(Action),
+easy_cpu_select_ship_action(Ships, PlayerNo, ShipNo,TradeStations, Colonies,  NewTradeStations, NewColonies) :-
+  easy_cpu_select_action(ReadAction),
+  convert_number_to_action(ReadAction,Action),
   valid_action(Action, PlayerNo, TradeStations, Colonies),!,
   perform_action(Ships, PlayerNo, ShipNo, Action, TradeStations, Colonies, NewTradeStations, NewColonies).
 
-select_ship_action(Ships, PlayerNo, ShipNo, TradeStations, Colonies,  NewTradeStations, NewColonies) :-
-  select_ship_action(Ships, PlayerNo, ShipNo, TradeStations, Colonies,  NewTradeStations, NewColonies).
+easy_cpu_select_ship_action(Ships, PlayerNo, ShipNo, TradeStations, Colonies,  NewTradeStations, NewColonies) :-
+  easy_cpu_select_ship_action(Ships, PlayerNo, ShipNo, TradeStations, Colonies,  NewTradeStations, NewColonies).
+
+%selectors
+easy_cpu_select_ship(NumShipsPerPlayer, ShipNo) :-
+    N1 is NumShipsPerPlayer + 1,
+    random(1, N1,ShipNo),
+    write('CPU chose ship '), write(ShipNo), nl.
+
+easy_cpu_select_ship_direction(Direction) :-
+    random(1,7,Direction),
+    write('CPU chose direction '), write(Direction), nl.
+
+easy_cpu_select_wormhole_exit(NumWormholes, InWormhole, OutWormhole) :-
+    N1 is NumWormholes),
+    random(0,N1,RandomOutWormhole),
+    RandomOutWormhole \= InWormhole,
+    SelectedOutWormhole is RandomOutWormhole,
+    write('CPU chose Wormhole '), write(SelectedOutWormhole), nl,
+    list_get_nth(Wormholes, SelectedOutWormhole, OutWormhole).
+
+easy_cpu_select_wormhole_exit(NumWormholes, InWormhole, OutWormhole) :-
+    easy_cpu_select_wormhole_exit(NumWormholes, InWormhole, OutWormhole).
+
+easy_cpu_select_action(Action) :-
+    random(0,2,Action),
+    write('CPU chose action '), write(Action), nl.
+
+easy_cpu_select_ship_num_tiles(NumTiles) :-
+    random(1,7, NumTiles),
+    write('CPU chose '), write(NumTiles), write( ' tiles'), nl.
--- tp1/sse.pl
+++ tp1/sse.pl
@@ -1,6 +1,7 @@
 :-include('logic/logic.pl').
 :-include('logic/logic_utils.pl').
 :-include('logic/point_calculation.pl').
+:-include('logic/easy_cpu.pl').
 :-include('interface/display_board.pl').
 :-include('interface/interface.pl').
 :-include('general_utils.pl').
@@ -14,10 +15,16 @@ play(Board, Ships, TradeStations, Colonies, HomeSystems, Wormholes, NumPlayers, 
   play(Board, Ships, TradeStations, Colonies, HomeSystems, Wormholes, NumPlayers, NumShipsPerPlayer, 0).
 
 play(Board, Ships, TradeStations, Colonies, HomeSystems, Wormholes, NumPlayers, NumShipsPerPlayer, CurrentPlayer):-
+    CurrentPlayer = 0,
   display_board(Board, Ships, TradeStations, Colonies),
   select_ship_movement(Board, Ships, TradeStations, Colonies, Wormholes, NumPlayers, NumShipsPerPlayer, CurrentPlayer, NewShips, ShipNo),
   display_board(Board, NewShips, TradeStations, Colonies),
   select_ship_action(NewShips, CurrentPlayer, ShipNo, TradeStations, Colonies, NewTradeStations, NewColonies),
+  check_game_state(Board, NewShips, NewTradeStations, NewColonies, HomeSystems, Wormholes, NumPlayers, NumShipsPerPlayer, CurrentPlayer).
+
+play(Board, Ships, TradeStations, Colonies, HomeSystems, Wormholes, NumPlayers, NumShipsPerPlayer, CurrentPlayer):-
+  easy_cpu_select_ship_movement(Board, Ships, TradeStations, Colonies, Wormholes, NumPlayers, NumShipsPerPlayer, CurrentPlayer, NewShips, ShipNo),
+  easy_cpu_select_ship_action(NewShips, CurrentPlayer, ShipNo, TradeStations, Colonies, NewTradeStations, NewColonies),
   check_game_state(Board, NewShips, NewTradeStations, NewColonies, HomeSystems, Wormholes, NumPlayers, NumShipsPerPlayer, CurrentPlayer).
 
 check_game_state(Board, Ships, TradeStations, Colonies, HomeSystems, Wormholes, NumPlayers, NumShipsPerPlayer, CurrentPlayer):-
